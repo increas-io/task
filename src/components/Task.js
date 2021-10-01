6debb1399647dc5234717088d4961b811c96340a
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-10-01 13:09:17 +0300

    title mt fixed

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,7 +7,7 @@ export default function Task() {
       <>
         <div className="flex w-full">
           <div className="w-2/4">
-            <h1 className="mt-20 ml-28 text-8xl font-bold">Component</h1>
+            <h1 className="mt-12 ml-28 text-8xl font-bold">Component</h1>
             <p className="mt-20 ml-28 text-3xl text-gray-500">My component &#60;Task&#62;</p>
           </div>
           <div class="flex flex-col">
